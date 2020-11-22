e41d47b1ae5764a2a819642c8ec92d7bc758721b
HobbyEd <edwinvandillen@gmail.com>
2020-11-22 17:01:57 +0100

    Eerste opzet

---
+++ InstallBasicApps.ps1
@@ -0,0 +1,23 @@
+winget.exe install Google.Chrome
+
+winget.exe install Microsoft.VisualStudioCode
+winget.exe install Microsoft.VisualStudio.Professional
+winget.exe install Microsoft.WindowsTerminal
+winget.exe install Notepad++
+winget.exe install Docker.Desktop
+winget.exe install Postman.Postman 
+winget.exe install Microsoft.PowerToys
+winget.exe install Microsoft.AzureCLI
+winget.exe install SQLServerManagementStudio
+
+winget.exe install Spotify
+winget.exe install VideoLAN.VLC
+winget.exe install Microsoft.Teams 
+winget.exe install WhatsApp.WhatsApp
+
+
+Write-Output "Zelf installeren:" 
+Start-Process chrome https://keepass.info
+Start-Process chrome https://www.sonos.com
+Start-Process chrome https://www.dotpdn.com/downloads/pdn.html
+Start-Process chrome https://www.bizagi.com/en/platform/modeler
