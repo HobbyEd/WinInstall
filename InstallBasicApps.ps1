fda80e40fe1a150eba75dec7fae5151779442dc2
HobbyEd <edwinvandillen@gmail.com>
2020-11-23 09:53:10 +0100

    Silent mode toegevoegd

--- InstallBasicApps.ps1
+++ InstallBasicApps.ps1
@@ -1,61 +1,61 @@
 Clear-Host
 Write-Output "Install: Google Chrome"
-winget.exe install Google.Chrome
+winget.exe install -h Google.Chrome
 
 Write-Output "Install: Git"
-winget.exe install Git.Git
+winget.exe install -h Git.Git
 
 Write-Output "Install: Visual Studio Code"
-winget.exe install Microsoft.VisualStudioCode
+winget.exe install -h Microsoft.VisualStudioCode
 
 Write-Output "Install: Visual Studio Professional"
-winget.exe install Microsoft.VisualStudio.Professional
+winget.exe install -h Microsoft.VisualStudio.Professional
 
 Write-Output "Install: Windows Terminal"
-winget.exe install Microsoft.WindowsTerminal
+winget.exe install -h Microsoft.WindowsTerminal
 
 Write-Output "Install: Notepad++"
-winget.exe install Notepad++
+winget.exe install -h Notepad++
 
 Write-Output "Install: Docker Desktop"
-winget.exe install Docker.Desktop
+winget.exe install -h Docker.Desktop
 
 Write-Output "Install: Postman"
-winget.exe install Postman.Postman 
+winget.exe install -h Postman.Postman 
 
 Write-Output "Install: PowerToys"
-winget.exe install Microsoft.PowerToys
+winget.exe install -h Microsoft.PowerToys
 
 Write-Output "Install: Azure CLI"
-winget.exe install Microsoft.AzureCLI
+winget.exe install -h Microsoft.AzureCLI
 
 Write-Output "Install: Microsoft SQL Server Management Studio"
-winget.exe install SQLServerManagementStudio
+winget.exe install -h SQLServerManagementStudio
 
 
 #Write-Output "Install: Oracle Virtual Box"
-#winget.exe install Oracle.VirtualBox
+#winget.exe install -h Oracle.VirtualBox
 
 Write-Output "Install: Spotify"
-winget.exe install Spotify
+winget.exe install -h Spotify
 
 Write-Output "Install: VLC"
-winget.exe install VideoLAN.VLC
+winget.exe install -h VideoLAN.VLC
 
 Write-Output "Install: Mircrosoft Teams"
-winget.exe install Microsoft.Teams 
+winget.exe install -h Microsoft.Teams 
 
 Write-Output "Install: Whatsapp Desktop"
-winget.exe install WhatsApp.WhatsApp
+winget.exe install -h WhatsApp.WhatsApp
 
 Write-Output "Install: MatterMost client"
-winget.exe install Mattermost.MattermostDesktop
+winget.exe install -h Mattermost.MattermostDesktop
 
 Write-Output "Install: Google backup and sync"
-winget.exe install Google.BackupAndSync
+winget.exe install -h Google.BackupAndSync
 
 Write-Output "Install: Google file stream"
-winget.exe install Google.DriveFileStream
+winget.exe install -h Google.DriveFileStream
 
 Write-Output "Zelf installeren:" 
 Start-Process chrome https://keepass.info
